2caf80e65f03f30c8bbee51857cd7cd4c6d2d2e9
Johnny Lay <86290365+johlay@users.noreply.github.com>
2023-04-14 14:52:34 +0200

    main run function

--- run.py
+++ run.py
@@ -1,6 +1,15 @@
+from spotify_client import SpotifyClient
+from dotenv import load_dotenv
+from pathlib import Path
+
 
 def run():
-    pass
+    load_dotenv(dotenv_path=Path("credentials/.env"))
+
+    spotify_client_instance = SpotifyClient()
+    spotify_client_instance.get_spotify_access_token()
+    spotify_client_instance.get_spotify_authorization_token()
+
 
 if __name__ == "__main__":
     run()
